b9f92204d5ec04cea9746624d4b00cd399f78585
Valeriy Gurnak <vmgurnak@gmail.com>
2025-09-16 12:14:23 +0300

    our-products

---
+++ src/js/swipper.js
@@ -0,0 +1,38 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  slidesPerGroup: 1,
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 18,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 18,
+    },
+  },
+});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import 'modern-normalize';
 
-import './js/mobile-menu';
-import './js/modal';
-import './js/play-video';
+// import './js/mobile-menu';
+// import './js/modal';
+import './js/swipper';
